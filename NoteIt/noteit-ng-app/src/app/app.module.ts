39bc1453a8d6e54c4659be53e05e040bdb5def36
Stephan Weiß <Stephan.Weiss@adesso.de>
2019-03-18 18:24:55 +0100

    more new components

--- NoteIt/noteit-ng-app/src/app/app.module.ts
+++ NoteIt/noteit-ng-app/src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+const appRoutes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent
+  },
+  {
+    path: '',
+    component: NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ NoteIt/noteit-ng-app/src/app/feedback/feedback.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.css']
+})
+export class FeedbackComponent implements OnInit {
+  model: FeedbackViewModel = {
+    name: '',
+    email: '',
+    feedback: ''
+  };
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+  }
+
+  sendFeedback(): void {
+    const url = 'http://localhost:8082/api/feedback';
+    this.http.post(url, this.model).subscribe(
+      res => {
+        location.reload();
+      },
+      err => {
+        alert('An error has occurred while sending feedback!');
+      }
+    );
+  }
+
+}
+
+export interface FeedbackViewModel {
+  name: string;
+  email: string;
+  feedback: string;
+}
